90d7e8228c38e289c85b85bbe7f569e28de78d08
Dmitry Fedorov <dima050595fedorov@gmail.com>
2022-10-27 22:33:49 +0300

    Реализовал поиск

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -2,34 +2,51 @@ import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import MovieServise from '../../servises/MovieServise';
+import SearchPanel from '../SearchPanel';
 
 import Spinner from './Spinner';
 import ErrorMessage from './ErrorMessage';
 import MoviesView from './MoviesView';
 
 class Movies extends Component {
-  movieRequest = new MovieServise();
+  MovieServise = new MovieServise();
 
   state = {
     movieData: null,
     imageURL: 'https://image.tmdb.org/t/p/original',
-    loading: true,
+    loading: false,
     error: false,
     errorNetwork: false,
+    query: null,
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updateSearchMovies();
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const { query } = this.state;
+    if (query !== prevState.query) {
+      this.updateSearchMovies();
+    }
+  }
+
   onLoadMovies = (movies) => {
-    this.setState(() => {
-      return {
-        movieData: movies,
-        loading: false,
-      };
-    });
+    if (movies.length !== 0) {
+      this.setState(() => {
+        return {
+          movieData: movies,
+          loading: false,
+        };
+      });
+    } else {
+      this.setState(() => {
+        return {
+          movieData: null,
+          loading: false,
+        };
+      });
+    }
   };
 
   onError = (e) => {
@@ -49,26 +66,51 @@ class Movies extends Component {
   };
 
   updateSearchMovies = () => {
-    this.movieRequest.getSearchMovies('return').then(this.onLoadMovies).catch(this.onError);
+    const { query } = this.state;
+    if (!query) {
+      return;
+    }
+    this.setState(() => {
+      return {
+        loading: true,
+      };
+    });
+    this.MovieServise.getSearchMovies(query).then(this.onLoadMovies).catch(this.onError);
+  };
+
+  queryValue = (e) => {
+    const newQuery = e.target.value;
+    this.setState(() => {
+      return {
+        query: newQuery,
+      };
+    });
   };
 
   // eslint-disable-next-line consistent-return
   render() {
     const { movieData, imageURL, loading, error, errorNetwork } = this.state;
 
-    const hasData = !(error || loading);
+    const hasData = !error && !loading;
     const errorMessage = [error ? <ErrorMessage key={uuidv4()} errorNetwork={errorNetwork} /> : null];
     const spin = [loading ? <Spinner key={uuidv4()} /> : null];
+    const noneContent = [hasData && !movieData ? <h1 key={uuidv4()}>По вашему запросу ничего не найдено</h1> : null];
     const content = [
-      hasData ? <MoviesView movieData={movieData} imageURL={imageURL} loading={loading} key={uuidv4()} /> : null,
+      hasData && movieData ? (
+        <MoviesView imageURL={imageURL} loading={loading} movieData={movieData} key={uuidv4()} />
+      ) : null,
     ];
 
     return (
-      <div className="Movies">
-        {errorMessage}
-        {spin}
-        {content}
-      </div>
+      <>
+        <SearchPanel queryValue={(e) => this.queryValue(e)} />
+        <div className="Movies">
+          {errorMessage}
+          {spin}
+          {noneContent}
+          {content}
+        </div>
+      </>
     );
   }
 }
---
+++ src/components/SearchPanel/SearchPanel.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Input } from 'antd';
+
+import './SearchPanel.css';
+
+const searchValue = (e, queryValue) => {
+  queryValue(e);
+};
+
+const debounce = (fn, debounceTime) => {
+  let time;
+  return function times(...args) {
+    clearTimeout(time);
+    time = setTimeout(() => {
+      fn.apply(this, args);
+    }, debounceTime);
+  };
+};
+
+const debouncedFn = debounce(searchValue, 800);
+
+function SearchPanel({ queryValue }) {
+  return (
+    <div className="SearchPanel">
+      <Input className="SearchPanel" placeholder="Type to search..." onChange={(e) => debouncedFn(e, queryValue)} />
+    </div>
+  );
+}
+
+export default SearchPanel;
